a243e241a055539bc308a1a417c75a901b5da60c
TJ Slezak <tjslezak@hotmail.com>
2018-03-25 21:59:07 -0700

    Modified to include additional requirements
    Implements function to count files and loop statement.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
+function nfpwd () {
+ls | wc -l
+}
+
+nf="$(nfpwd)"
+#echo $nf
+
 echo "Guess how many files are in the current directory and then press Enter:"
 read response
-pwdir = echo | pwd
-nfpwdir = grep pwd | wc -l
 
-if [[$response -eq nfpwdir]] 
+if [[$response -eq $nf]] 
 then
 	echo "You guessed correctly!"
-elif [[$response -gt nfpwdir]]
+elif [[$response -gt $nf]]
 then
 	echo "You guessed too high. Guess again!"
-elif [[$response -lt nfpwdir]]
+elif [[$response -lt $nf]]
 then
 	echo "You guessed too low. Guess again!"
 else 
 	echo "You guessed incorrectly. Guess again!"
 fi
 
+while [ $response -ne $nf ]
+do
+echo "Enter your guess: "
+
+done 
--- makefile
+++ makefile
@@ -1,4 +1,11 @@
-README.md:
-	echo "Peer-graded Asssignment: Bash, Make, Git, and Github"
-	echo >> dt$(date '+%d/%m/%Y %H:%M:%S')
-	echo >> egrep guessinggame.sh -lc
+README.md: guessinggame.sh
+	echo "# Peer-graded Asssignment: Bash, Make, Git, and Github" > README.md
+	#echo "---\n" >> README.md	
+
+	echo "\n## Created " >> README.md
+	date -u >> README.md
+
+	echo "\n## Lines of code in game: " >> README.md 
+	wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md
+
+	echo "\n---\n" >> README.md
